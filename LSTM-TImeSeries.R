fe75aa432b28e02c4377ec78b6233ef5f983ea4d
Arne Rustad <ArneRustad@users.noreply.github.com>
2021-10-13 15:50:36 +0200

    Update lstm model to include batch specification and different train and test bacth sizes (fixed using copying of weights to an almost identical model)

--- LSTM-TImeSeries.R
+++ LSTM-TImeSeries.R
@@ -21,21 +21,20 @@ data$Price = as.numeric(as.character(data$Price))
 scale_factors <- c(mean(data$Price, na.rm=TRUE), sd(data$Price, na.rm=TRUE))
 scale_factors
 
-
-
 scaled_price <- data %>%
   dplyr::select(Price) %>%
   dplyr::mutate(Price = (Price - scale_factors[1]) / scale_factors[2])
 
-scaled_train <- scaled_price[data$Date<as.Date("2018-01-01"),]
+start.date.test = as.Date("2020-01-01")
+scaled_train <- scaled_price[data$Date < start.date.test,]
+scaled_test <- scaled_price[data$Date >= start.date.test,]
 
-scaled_test <- scaled_price[data$Date>=as.Date("2018-01-01"),]
+length(scaled_train)
+length(scaled_test)
 
-prediction <- 12
-lag <- 12
-
-
-
+prediction <- 5
+lag <- 1
+batch_size = 50
 
 # we lag the data 11 times and arrange that into columns
 get_x_data <- function(scaled_data){
@@ -79,67 +78,53 @@ get_y_data <- function(scaled_data){
 x_train_arr = get_x_data(scaled_train)
 y_train_arr = get_y_data(scaled_train)
 
-class(scaled_test)
-class(scaled_train)
 x_pred_arr = get_x_data(scaled_test)
 y_pred_arr_truth = get_y_data(scaled_test)
 
+x_arr = get_x_data(scaled_price)
+y_arr = get_x_data(scaled_price)
 
 
+create_lstm_model = function(batch_size) {
+  lstm_model <- keras_model_sequential()
+  
+  lstm_model %>%
+    layer_lstm(units = 50, # size of the layer
+               batch_input_shape = c(batch_size, lag, 1), # batch size, timesteps, features
+               return_sequences = TRUE,
+               stateful = TRUE) %>%
+    # fraction of the units to drop for the linear transformation of the inputs
+    #layer_dropout(rate = 0.5) %>%
+    layer_lstm(units = 50,
+               return_sequences = TRUE,
+               stateful = TRUE) %>%
+    #layer_dropout(rate = 0.5) %>%
+    time_distributed(keras::layer_dense(units = 1))
+  
+  lstm_model %>%
+    compile(loss = 'mae', optimizer = 'adam', metrics = 'mae')
+  
+  return(lstm_model)
+}
 
-lstm_model <- keras_model_sequential()
+lstm_model = create_lstm_model(batch_size)
 
+train.indices.to.use = 1:(batch_size * floor(dim(x_train_arr)[[1]] / batch_size))
 
-lstm_model %>%
-  layer_lstm(units = 50, # size of the layer
-             batch_input_shape = c(1, 12, 1), # batch size, timesteps, features
-             return_sequences = TRUE,
-             stateful = TRUE) %>%
-  # fraction of the units to drop for the linear transformation of the inputs
-  #layer_dropout(rate = 0.5) %>%
-  layer_lstm(units = 50,
-             return_sequences = TRUE,
-             stateful = TRUE) %>%
-  #layer_dropout(rate = 0.5) %>%
-  time_distributed(keras::layer_dense(units = 1))
-
-lstm_model %>%
-  compile(loss = 'mae', optimizer = 'adam', metrics = 'mae')
-
-summary(lstm_model)
-
-# #LSTM en gang til
-# lstm_model_pred <- keras_model_sequential()
-# 
-# lstm_model_pred %>%
-#   layer_lstm(units = 50, # size of the layer
-#              batch_input_shape = c(1, 12, 1), # batch size, timesteps, features
-#              return_sequences = TRUE,
-#              stateful = TRUE) %>%
-#   # fraction of the units to drop for the linear transformation of the inputs
-#   #layer_dropout(rate = 0.5) %>%
-#   layer_lstm(units = 50,
-#              return_sequences = TRUE,
-#              stateful = TRUE) %>%
-#   #layer_dropout(rate = 0.5) %>%
-#   time_distributed(keras::layer_dense(units = 1))
-# 
-# lstm_model_pred %>%
-#   compile(loss = 'mae', optimizer = 'adam', metrics = 'accuracy')
-
-#Fit
 lstm_model %>% fit(
-  x = x_train_arr,
-  y = y_train_arr,
-  batch_size = 1,
-  epochs = 20,
+  x = array(x_train_arr[train.indices.to.use,,], dim = c(length(train.indices.to.use),lag,1)),
+  y = array(y_train_arr[train.indices.to.use,,], dim = c(length(train.indices.to.use),lag,1)),
+  batch_size = batch_size,
+  epochs = 40,
   verbose = 1,
   shuffle = FALSE
 )
 
-#lstm_model_pred$set_weights = lstm_model$get_weights
+lstm_model_pred = lstm_model
 
-#summary(lstm_model_pred)
+lstm_model_pred = create_lstm_model(1)
+lstm_model_pred$set_weights(lstm_model$get_weights())
+lstm_model_pred$weights[[2]] - lstm_model$weights[[2]]
 
 lstm_forecast <- lstm_model_pred %>%
   predict(x_pred_arr, batch_size = 1) %>%
@@ -150,14 +135,20 @@ lstm_forecast <- lstm_forecast * scale_factors[2] + scale_factors[1]
 lstm_forecast
  
 apply(abs(lstm_forecast-(y_pred_arr_truth[,,1]* scale_factors[2] + scale_factors[1])),2,mean)
-plot(lstm_forecast[,1], type="l")
+
 plot(y_pred_arr_truth[,1,1]* scale_factors[2] + scale_factors[1], type = "l")
 lines(lstm_forecast[,1])
-# lstm_forecast2 <- lstm_model %>%
-#   predict(x_pred_arr2, batch_size = 601) %>%
-#   .[, , 1]
-# 
-# # we need to rescale the data to restore the original values
-# lstm_forecast2 <- lstm_forecast2* scale_factors[2] + scale_factors[1]
-# lstm_forecast2[1,]
+
+
+lstm_forecast <- lstm_model_pred %>%
+  predict(x_train_arr, batch_size = 1) %>%
+  .[, , 1]
+# we need to rescale the data to restore the original values
+lstm_forecast <- lstm_forecast * scale_factors[2] + scale_factors[1]
+lstm_forecast
+
+apply(abs(lstm_forecast-(y_train_arr[,,1]* scale_factors[2] + scale_factors[1])),2,mean)
+plot(lstm_forecast[,1], type="l")
+plot(y_train_arr[,1,1]* scale_factors[2] + scale_factors[1], type = "l")
+lines(lstm_forecast[,1])
 
