1af10fca457d1ab930359dd99d16a4f41ee088cd
Arne Rustad <ArneRustad@users.noreply.github.com>
2021-10-15 10:49:49 +0200

    Added test statistics for arma predictions

--- exploring_data.R
+++ exploring_data.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(data.table)
-
+library(stringr)
 # Fetch data
 data= read.csv("bitcoin_data.csv")
 
@@ -115,3 +115,86 @@ ggplot(data.frame(Residual = mod.arima.log.differenced.best$residuals),
   xlab("Residual no.") + ylab("Value")
 ## ggsave
 
+order = c(1,1,2)
+start.date.test = "2019-01-01"
+transformation = log
+inv.transformation = exp
+n.pred.ahead = 5
+plot.pred = TRUE
+
+test.preds.arima = function(data, order, start.date.test, transformation = NULL, inv.transformation = NULL, n.pred.ahead = 1,
+                            plot.pred = NA) {
+  start.date.test = as.Date(start.date.test)
+  if (sum(is.null(c(transformation, inv.transformation))) == 1) {
+    stop("Either both or neither of the transformation and inv.transformation parameter must be entered")
+  }
+  if(!is.null(transformation)) data = data %>% mutate(Price = transformation(Price))
+  data.test = data[data$Date >= start.date.test,]
+  data.test[, paste0("Pred", 1:n.pred.ahead)] = NA
+  pred.cols = str_detect(colnames(data.test), "^Pred")
+  curr.date.test = start.date.test
+  for (i in seq_len(nrow(data.test))) {
+    fit = arima(data$Price[data$Date < curr.date.test], order = order)
+    data.test[i, pred.cols] = predict(fit, n.ahead = n.pred.ahead)$pred
+    curr.date.test = curr.date.test + 1
+  }
+  cols.pred.and.price = str_detect(colnames(data.test), "^Pred|^Price")
+  
+  for(i in 1:n.pred.ahead) {
+    pred.col = paste0("Pred", i)
+    pred.i = data.test[, pred.col]
+    rows.to.remove = length(pred.i) + 1 - seq_len(i)
+    data.test[, pred.col] = c(rep(NA, i), pred.i[-rows.to.remove])
+  }
+  
+  if(!is.null(inv.transformation)) {
+    data.test[, cols.pred.and.price] = inv.transformation(data.test[, cols.pred.and.price])
+    data = data %>% mutate(Price = inv.transformation(Price)) 
+  }
+  
+  if(!is.na(plot.pred)) {
+    p = ggplot(data.test, aes(x = Date, y = Pred1, col = "Pred")) + geom_line() +
+      guides(col = guide_legend(title = "Line")) + ylab(paste0("Pred", plot.pred)) +
+      ggtitle(sprintf("Prediction %d days ahead for ARMA model plotted against true Bitcoin price", plot.pred))
+    p = p + geom_line(data = data, aes(x = Date, y = Price, col = "Truth"))
+    print(p)
+  }
+  
+  df.error = data.frame("Pred_Days_Ahead" = 1:n.pred.ahead,
+                        mse = apply(data.test[,pred.cols], 2,
+                                    function(x) return(mean((x - data.test$Price)^2, na.rm = TRUE))),
+                        mae = apply(data.test[,pred.cols], 2,
+                                    function(x) return(mean(abs(x - data.test$Price), na.rm = TRUE))))
+  
+  true.price.change = diff(data.test$Price)
+  pred.price.change = data$Price[data$Date <= (start.date.test-1)]
+  df.direction = data.frame("Pred_Days_Ahead" = 1:n.pred.ahead, Correct_direction = NA, True_direction_upwards = NA)
+  df.investment = data.frame("Pred_Days_Ahead" = 1:n.pred.ahead, Passive = NA, Active = NA)
+  df.investment[, "Passive"] = tail(data.test$Price, 1) / head(data.test$Price, 1)
+  for (i in 1:nrow(df.direction)) {
+    pred.direction = sign(head(data.test$Price, -i) - tail(data.test[, paste0("Pred", i)], -i))
+    true.difference = diff(data.test$Price, lag = i)
+    true.direction = sign(true.difference)
+    correct.direction.pred = true.direction == pred.direction
+    
+    df.direction[i, "Correct_direction"] = mean(correct.direction.pred, na.rm = TRUE)
+    df.direction[i, "True_direction_upwards"] = mean(true.direction == 1, na.rm = TRUE)
+    
+    true.difference.percent = true.difference / head(data.test$Price, -i)
+    if (i == 1) {
+      true.difference.percent.1.day.ahead = true.difference.percent
+      df.investment[i, "Active"] =  prod(1 + ifelse(pred.direction == 1, true.difference.percent, 0), na.rm = TRUE)
+    } else {
+      df.investment[i, "Active"] =  prod(1 + ifelse(pred.direction == 1, head(true.difference.percent.1.day.ahead, -(i-1)), 0), na.rm = TRUE)
+    }
+  }
+  df.investment
+  df.direction
+  return(list(df.error = df.error, df.direction = df.direction, df.investment))
+}
+
+test.preds.arima(data, order = c(2,1,3), start.date.test = "2019-01-01",
+                 transformation = log, inv.transformation = exp,
+                 n.pred.ahead = 2, plot.pred = TRUE)
+is.null(exp)
+sum(is.na(c(log, exp)))
