1b2f9cb7135435ad95c25fe42794adda53295421
Elsie Tandberg <elsie.tandberg@gmail.com>
2021-11-23 11:13:14 +0100

    experimenting with ugarch package

--- Project2/unit_root.R
+++ Project2/unit_root.R
@@ -5,13 +5,22 @@ adf.test(log(na.omit(data$Price)))
 pp.test(log(na.omit(data$Price)))
 
 fit.garch<-garchFit(~garch(1,0), diff(log(na.omit(data$Price))))
-summary(garch)
+summary(fit.garch)
 
-model2.spec <- ugarchspec(variance.model = list(garchOrder=c(1,1)),mean.model = list(armaOrder = c(1, 1),include.mean=FALSE))
+model2.spec <- ugarchspec(variance.model = list(garchOrder=c(1,1)),mean.model = list(armaOrder = c(0, 0),include.mean=FALSE))
 model2=ugarchfit(spec=model2.spec, data=diff(log(na.omit(data$Price))))
-summary(model2)
+#summary(model2)
 infocriteria(model2)
 
-?ugarchspec
 
-fit.garch@fit
+spec = ugarchspec()
+fit = ugarchfit(data=diff(log(na.omit(data$Price))),spec=spec)
+infocriteria(fit)
+
+
+model3.spec <- ugarchspec(variance.model = list(garchOrder=c(1,1), model="fGARCH", submodel="GARCH"),mean.model = list(armaOrder = c(0, 0),include.mean=FALSE))
+model3=ugarchfit(spec=model3.spec, data=diff(log(na.omit(data$Price))))
+#summary(model2)
+infocriteria(model3)
+getMethod("infocriteria", "uGARCHfit")
+
