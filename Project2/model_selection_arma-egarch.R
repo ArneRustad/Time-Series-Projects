6c159809742702e422bc8a3ce02316ebbfede661
Lene Omdal <lene.omdal@hotmail.com>
2021-11-23 18:59:09 +0100

    Add egarch and igarch

--- Project2/model_selection_arma-egarch.R
+++ Project2/model_selection_arma-egarch.R
@@ -6,7 +6,6 @@ max.q = 2
 max.garch.p = 2
 max.garch.q = 2
 df.aic.log.differenced = data.table(expand.grid(ar = 0:max.p, ma = 0:max.q, garch.p = 0:max.garch.p, garch.q = 0:max.garch.q))
-View(df.aic.log.differenced)
 
 pb = progress_bar$new(format = "(:spin) [:bar] :percent [Elapsed time: 
                       :elapsedfull || Estimated time remaining: :eta]",
@@ -21,7 +20,7 @@ for (i in 1:nrow(df.aic.log.differenced)) {
   tryCatch(
     {
       model.spec <- ugarchspec(variance.model = list(garchOrder=c(df.aic.log.differenced$garch.p[i], df.aic.log.differenced$garch.q[i]), 
-                                                     model="fGARCH", submodel="GARCH"),
+                                                     model="eGARCH"),
                                mean.model = list(armaOrder = c(df.aic.log.differenced$ar[i], df.aic.log.differenced$ma[i]),include.mean=FALSE),
                                distribution.model = "std")
       model=ugarchfit(spec=model.spec, data=diff(log(na.omit(data$Price))))
@@ -40,14 +39,14 @@ for (i in 1:nrow(df.aic.log.differenced)) {
 
 df.aic.log.differenced = df.aic.log.differenced[order(df.aic.log.differenced$BIC, decreasing = FALSE),]
 df.aic.log.differenced
-fwrite(df.aic.log.differenced, paste0(result.dir, "df_aic_log_diff_arma-garch_tdist.csv"))
-df.aic.log.differenced = fread(paste0(result.dir, "df_aic_log_diff_arma-garch_tdist.csv"))
+fwrite(df.aic.log.differenced, paste0(result.dir, "df_aic_log_diff_arma-egarch_tdist.csv"))
+df.aic.log.differenced = fread(paste0(result.dir, "df_aic_log_diff_arma-egarch_tdist.csv"))
 df.aic.log.differenced
 
 
 # Fitting best GARCH model
 model.spec.best.garch <- ugarchspec(variance.model = list(garchOrder=c(df.aic.log.differenced$garch.p[1], df.aic.log.differenced$garch.q[1]), 
-                                                          model="fGARCH", submodel="GARCH"),
+                                                          model="eGARCH"),
                                     mean.model = list(armaOrder = c(df.aic.log.differenced$ar[1] ,df.aic.log.differenced$ma[1]), include.mean=FALSE),
                                     distribution.model = "std")
 
@@ -75,13 +74,13 @@ df.log.diff.garch = data.frame(Date = dplyr::filter(data.log.diff, Date >= start
                                upper.confint = pred + half.confint.length)
 df.log.diff.garch.long = pivot_longer(df.log.diff.garch, -Date, names_to = "Line_all")
 df.log.diff.garch.long$Line = str_replace(df.log.diff.garch.long$Line_all, "lower.confint|upper.confint", paste(1 - alpha, "confint"))
-fwrite(df.log.diff.garch.long, paste0(result.dir, "df_log_diff_arma-garch_long.csv"))
+fwrite(df.log.diff.garch.long, paste0(result.dir, "df_log_diff_arma-egarch_tdist_long.csv"))
 
 
 ggplot(df.log.diff.garch.long, aes(x = Date)) + geom_line(aes(y = value, col = Line, group = Line_all)) +
   geom_line(aes(y = Price, col = "Truth"), data = data.log.diff, alpha = 0.5) +
-  ggtitle("ARMA-GARCH 0.95 prediction intervals for diff log price") + ylab("Diff(log(Price))")
+  ggtitle("ARMA-EGARCH 0.95 prediction intervals for diff log price") + ylab("Diff(log(Price))")
 
 
 print(model.best.garch@fit$coef)
-ggsave("arma-garch_log_diff_pred_confint.jpg", path = image.dir, width = img.width, height = img.height)
+ggsave("arma-egarch_tdist_log_diff_pred_confint.jpg", path = image.dir, width = img.width, height = img.height)
